a5950759a7dcc471e7f723c1f6b325f1345ead75
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-04-08 23:15:50 +0300

    Created freeCargo.tsx

---
+++ src/components/badge/freeCargo.tsx
@@ -0,0 +1,27 @@
+//import liraries
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {RouteType} from '../routes/RouteType';
+
+type Props = RouteType<'FreeCargo'>;
+// create a component
+const FreeCargo: React.FC<Props> = () => {
+  return (
+    <View style={styles.container}>
+      <Text>MyComponent</Text>
+    </View>
+  );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2c3e50',
+  },
+});
+
+//make this component available to the app
+export default FreeCargo;
