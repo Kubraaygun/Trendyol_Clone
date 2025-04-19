a16cdf51052f68640a6fb3f36ef1d0f894065ab2
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-04-19 16:14:51 +0300

    Backup before Xcode flag fix

--- src/store/actions/authActions.ts
+++ src/store/actions/authActions.ts
@@ -2,9 +2,11 @@ import {createAsyncThunk} from '@reduxjs/toolkit';
 import {postRequest} from '../../service/verbs';
 import {AUTH_URLS} from '../../service/urls';
 
+
 const userLogin = createAsyncThunk('auth/userLogin', async (params: object) => {
   try {
     const response = await postRequest(params, AUTH_URLS.LOGIN);
+  
 
     return response.data;
   } catch (error) {
